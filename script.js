42e49b0ed4a199fb406f5b088402dc800c73cce9
Locax-1 <3172086298@qq.com>
2026-06-13 19:07:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -370,7 +370,7 @@ if (analyzeBtn) {
 
         try {
             // --- 配置区域 ---
-            // 上线后请改为你的真实域名，例如: "https://my-api.vercel.app/api/query-range"
+            // 上线后请改为你的真实域名，例如: "https://my-api.vercel.app/api/"
             const API_URL = "https://el-ircv.vercel.app/api/query-range";
 
             // 2. 发送请求
